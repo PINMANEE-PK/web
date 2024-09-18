78fa31734d4b46fc558707073647b3ab2cb0edd9
PINMANEE-PK <pinmanee6462gh@gmail.com>
2024-09-18 14:57:49 +0700

    update

---
+++ myapp/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html'],
+  theme: {
+    extend: {
+      fontFamily :{
+        angsana:["Angsana New"],
+        sriracha:["Sriracha"],
+        itim:["Itim"]
+      }
+    },
+  },
+  plugins: [],
+}
+
